9580f3c59866ee32bf90790b096f54921a06123a
Arthur Fewell <afewell@gmail.com>
2023-09-24 09:07:38 -0700

    Create tmc_vars.tf

---
+++ terraform/tmc_vars.tf
@@ -0,0 +1,10 @@
+variable "endpoint" {
+  type        = string
+  description = "TMC endpoint"
+  default = "<put your TMC endpoint url>"
+}
+variable "vmw_cloud_api_token" {
+  type        = string
+  description = "TMC API Token"
+  default = "<put your token here>"
+}
